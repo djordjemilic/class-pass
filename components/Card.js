1df2b4f1c19ec4b8c244b737448c0fb69c209b73
Djordje <djordjemilic33@gmail.com>
2023-08-13 13:46:49 +0200

    Add styles for the Card component

--- components/Card.js
+++ components/Card.js
@@ -1,23 +1,24 @@
 import {media} from '@wix/api-client';
+
 const Card = ({service}) => {
     const image = media.getImageUrl(service.media.coverMedia.image)
 
     return (
-       <article className="card">
-           <div className="card-info__container">
-               <div className="card-img__container">
-                   <img src={image.url} alt={service.name}/>
-               </div>
-               <div className="card-text__container">
-                   <h3>{service.name}</h3>
-                   <p>{service.tagLine}</p>
-               </div>
-           </div>
-           <div>
-               <p className="card-description__container">{service.description}</p>
-           </div>
-       </article>
+        <article className="card">
+            <div className="card-info__container">
+                <div className="card-img__container">
+                    <img src={image.url} alt={service.name}/>
+                </div>
+                <div className="card-text__container">
+                    <h3>{service.name}</h3>
+                    <p>{service.tagLine}</p>
+                </div>
+            </div>
+            <div>
+                <p className="card-description__container">{service.description}</p>
+            </div>
+        </article>
     )
 }
 
-export  default Card;
+export default Card;
