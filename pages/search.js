c241e05186af0dd9b4b033c7d2c32d65b447f8ec
Djordje <djordjemilic33@gmail.com>
2023-08-13 13:14:18 +0200

    Fetch the data from wix dashboard

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,31 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import Cookies from 'js-cookie';
+import {services} from "@wix/bookings";
+import Link from "next/link";
+import {createClient, OAuthStrategy} from "@wix/api-client";
+
+const myWixClient = createClient({
+    modules: { services },
+    auth: OAuthStrategy({
+        clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID,
+        tokens: JSON.parse(Cookies.get('session') || '{}'),
+    })
+})
 
 const Search = () => {
+    const [serviceList, setServiceList] = useState([]);
+
+    const fetchServices = async () => {
+        const serviceList = await myWixClient.services.queryServices().find();
+        setServiceList(serviceList.items)
+    }
+
+    useEffect(() => {
+        fetchServices();
+    }, [])
+
+    console.log(serviceList)
+
     return (
         <div>
             <h1>Search</h1>
